132ea366b5f374fd38fa1be1b3401663f3e49836
foxbox10001 <117731701+foxbox10001@users.noreply.github.com>
2022-11-08 15:42:22 +0400

    Added full menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('New diner')
 streamlit.header('Breakfast Menu')
-streamlit.text("Omega & Bluebryy Oatmeal")
-streamlit.text("Kale Smoothie")
-streamlit.text("Free-range Egg")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
