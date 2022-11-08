b978165d321939799a8aac7796120840d5c3ae8f
foxbox10001 <117731701+foxbox10001@users.noreply.github.com>
2022-11-08 15:41:34 +0400

    Added a menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('New diner')
+streamlit.header('Breakfast Menu')
+streamlit.text("Omega & Bluebryy Oatmeal")
+streamlit.text("Kale Smoothie")
+streamlit.text("Free-range Egg")
