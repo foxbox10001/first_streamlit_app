7d4a2a601ea98821b8003e5bc2705ece48d0743d
foxbox10001 <117731701+foxbox10001@users.noreply.github.com>
2022-11-11 14:49:02 +0400

    Fixed typo in 39 row

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrers["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select current_user(), current_account(), current_region()")
 my_data_row = my_cur.fetchone()
