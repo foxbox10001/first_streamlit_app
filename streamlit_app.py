e2e738d2d82fd8735e21c435caa523509f49cbe0
foxbox10001 <117731701+foxbox10001@users.noreply.github.com>
2022-11-11 14:51:45 +0400

    Added select from fruit_load_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select current_user(), current_account(), current_region()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("From Snowflake:")
+streamlit.text("From Snowflake: the fruit load list contains:")
 streamlit.text(my_data_row)
