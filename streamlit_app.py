19306fb340602aebb8736ffb172e7264e5bf6615
foxbox10001 <117731701+foxbox10001@users.noreply.github.com>
2022-11-11 15:51:26 +0400

    Added function for insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,17 @@ except URLError as e:
   streamlit.error()
 
 
+# allow the end user to add a fruit from the list:
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')");
+    return "Thanks for adding", add_my_fruit
+
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+if streamlit.button('Add a fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  ctreamlit.text(back_from_function)
 
 streamlit.header("The fruit load list contains:")
 # snowflake function:
@@ -59,9 +70,3 @@ if streamlit.button('Get Fruit Load List'):
 
 streamlit.stop()
 
-# allow the end user to add a fruit from the list:
-add_my_fruit = streamlit.text_input("What fruit would you like to add?", "jackfruit")
-streamlit.write("Thanks for adding", add_my_fruit)
-
-# 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')");
